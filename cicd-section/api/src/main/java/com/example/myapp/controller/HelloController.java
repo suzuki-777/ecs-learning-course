57790e2705cd676ab73ddc0ae6c55bf32d3f3704
k sz <szk@kenoMacBook-Pro.local>
2026-08-10 17:50:13 +0900

    trial github actions

--- cicd-section/api/src/main/java/com/example/myapp/controller/HelloController.java
+++ cicd-section/api/src/main/java/com/example/myapp/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping
     String get() {
-        return "Hello";
+        return "Hello World";
     }
 }
